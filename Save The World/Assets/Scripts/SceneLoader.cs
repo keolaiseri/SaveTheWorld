ada074b083263cef9c6aaaa0cd911db0126c9994
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-05-12 12:45:04 -1000

    Added some assets

---
+++ Save The World/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+    public void LoadStartScene() {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 2);
+
+    }
+
+    public void QuitGame() {
+
+        Application.Quit();
+
+    }
+
+
+
+
+}
