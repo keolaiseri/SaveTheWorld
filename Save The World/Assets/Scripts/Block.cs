29e790d5258e7a93a688c21343cdea6e8e3d2b8a
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-06-15 13:20:45 -1000

    Destroy Block

--- Save The World/Assets/Scripts/Block.cs
+++ Save The World/Assets/Scripts/Block.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
+        Destroy(gameObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
