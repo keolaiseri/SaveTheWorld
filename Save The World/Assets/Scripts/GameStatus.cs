b22ef6e9634575987622c2e80037594a6def5c14
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-14 08:44:56 -1000

    displayed score

--- Save The World/Assets/Scripts/GameStatus.cs
+++ Save The World/Assets/Scripts/GameStatus.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     //Config Parameters
    [Range(0.1f, 10.0f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     //State Varibles
     [SerializeField] int currentScore = 0;
@@ -14,7 +16,8 @@ public class GameStatus : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = currentScore.ToString();
+            
     }
 
     // Update is called once per frame
@@ -26,6 +29,7 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 
 
